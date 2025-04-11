ea2a7d0c3cf935945a607d3cbe8337fcd58239a9
yanz1 <ellen.yan@vumc.org>
2025-04-11 15:46:10 -0500

    Added a prompt template Added a sample FAQ json

--- src/main/java/com/vumc/SpringAIDemo/OllamaController.java
+++ src/main/java/com/vumc/SpringAIDemo/OllamaController.java
@@ -4,17 +4,18 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.AssistantPromptTemplate;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -22,33 +23,46 @@ import java.util.Map;
 public class OllamaController {
     private ChatClient chatClient;
 
+    @Value("classpath:/prompts/context_prompt.st")
+    private Resource promptTemplateText;
+
+    @Value ("classpath:/docs/timeaway_faq.txt")
+    private Resource faqContentResource;
+
     public OllamaController(OllamaChatModel chatModel) {
         this.chatClient = ChatClient.create(chatModel);
     }
 
     @GetMapping("/{question}")
     public ResponseEntity<String> getAnswer(@PathVariable String question) {
-// User Role
+
         Message userMessage = new UserMessage(question);
-// System Role
-        String systemText = """
-                You are a helpful AI assistant that specialized in providing help for VUMC users who need to get answers regarding provider time away application.
-                Your name is TimeAway Assistant.
-                Be polite and respectful. Keep the conversation on topic and do not stray from what is available in the file.
-                  """;
-        Message systemMessage = new SystemPromptTemplate(systemText).createMessage();
+//// System Role
+//        String systemText = """
+//                You are a helpful AI assistant that specialized in providing help for VUMC users who need to get answers regarding provider time away application.
+//                Your name is TimeAway Assistant.
+//                Be polite and respectful. Keep the conversation on topic and do not stray from what is available in the file.
+//                  """;
+//        Message systemMessage = new SystemPromptTemplate(systemText).createMessage();
+//
+//        //Assistant Role
+//        String instructions = """
+//                       You are a helpful AI assistant that helps answer Provider Time Away related questions.
+//                       You should reply to the user's request with your name and a greeting.
+//                       If user's question is outside the Provider Time Away domain, please be polite and say that you can only answer PTA related questions.
+//                You should also inquire user's feedback at the end of conversation.
+//                        """;
+//        AssistantPromptTemplate assistantPromptTemplate = new AssistantPromptTemplate(instructions);
+//        Message assistantPromptTemplateMessage = assistantPromptTemplate.createMessage(Map.of("text", question));
 
-        //Assistant Role
-        String instructions = """
-                       You are a helpful AI assistant that helps answer Provider Time Away related questions.
-                       You should reply to the user's request with your name and a greeting. 
-                       If user's question is outside the Provider Time Away domain, please be polite and say that you can only answer PTA related questions.
-                You should also inquire user's feedback at the end of conversation.
-                        """;
-        AssistantPromptTemplate assistantPromptTemplate = new AssistantPromptTemplate(instructions);
-        Message assistantPromptTemplateMessage = assistantPromptTemplate.createMessage(Map.of("text", question));
+//        Prompt prompt = new Prompt(List.of(userMessage, systemMessage, assistantPromptTemplateMessage));
+        PromptTemplate promptTemplate = new PromptTemplate(promptTemplateText);
 
-        Prompt prompt = new Prompt(List.of(userMessage, systemMessage, assistantPromptTemplateMessage));
+        Map<String, Object> promptMap = new HashMap<String, Object>();
+        promptMap.put("question", question);
+        promptMap.put("context", faqContentResource);
+
+        Prompt prompt = promptTemplate.create(promptMap);
 
         ChatResponse chatResponse = chatClient
                 .prompt(prompt)

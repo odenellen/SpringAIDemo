758b6fc3bb05721f05eebbd63e9e075a9a8f1151
yanz1 <ellen.yan@vumc.org>
2025-04-14 07:38:13 -0500

    Added system, assistant messages Re-wrote the time away FAQ content

--- src/main/java/com/vumc/SpringAIDemo/OllamaController.java
+++ src/main/java/com/vumc/SpringAIDemo/OllamaController.java
@@ -2,10 +2,11 @@ package com.vumc.SpringAIDemo;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.AssistantPromptTemplate;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -15,19 +16,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/ollama")
 public class OllamaController {
-    private ChatClient chatClient;
+    private final ChatClient chatClient;
 
-    @Value("classpath:/prompts/context_prompt.st")
-    private Resource promptTemplateText;
+    @Value("classpath:/prompts/prompt.st")
+    private Resource promptTemplate;
 
-    @Value ("classpath:/docs/timeaway_faq.txt")
+    @Value ("classpath:/docs/faq.txt")
     private Resource faqContentResource;
+
+    @Value ("classpath:/docs/system.txt")
+    private Resource systemText;
+
+    @Value ("classpath:/docs/assistant.txt")
+    private Resource assistantText;
 
     public OllamaController(OllamaChatModel chatModel) {
         this.chatClient = ChatClient.create(chatModel);
@@ -36,40 +43,20 @@ public class OllamaController {
     @GetMapping("/{question}")
     public ResponseEntity<String> getAnswer(@PathVariable String question) {
 
-        Message userMessage = new UserMessage(question);
-//// System Role
-//        String systemText = """
-//                You are a helpful AI assistant that specialized in providing help for VUMC users who need to get answers regarding provider time away application.
-//                Your name is TimeAway Assistant.
-//                Be polite and respectful. Keep the conversation on topic and do not stray from what is available in the file.
-//                  """;
-//        Message systemMessage = new SystemPromptTemplate(systemText).createMessage();
-//
-//        //Assistant Role
-//        String instructions = """
-//                       You are a helpful AI assistant that helps answer Provider Time Away related questions.
-//                       You should reply to the user's request with your name and a greeting.
-//                       If user's question is outside the Provider Time Away domain, please be polite and say that you can only answer PTA related questions.
-//                You should also inquire user's feedback at the end of conversation.
-//                        """;
-//        AssistantPromptTemplate assistantPromptTemplate = new AssistantPromptTemplate(instructions);
-//        Message assistantPromptTemplateMessage = assistantPromptTemplate.createMessage(Map.of("text", question));
+        Message systemMessage = new SystemPromptTemplate(systemText).createMessage();
+        Message assistantMessage = new AssistantPromptTemplate(assistantText).createMessage();
 
-//        Prompt prompt = new Prompt(List.of(userMessage, systemMessage, assistantPromptTemplateMessage));
-        PromptTemplate promptTemplate = new PromptTemplate(promptTemplateText);
+        PromptTemplate promptTemplate = new PromptTemplate(this.promptTemplate);
+        Message promptMessage = promptTemplate.createMessage(Map.of("question", question, "context", faqContentResource));
 
-        Map<String, Object> promptMap = new HashMap<String, Object>();
-        promptMap.put("question", question);
-        promptMap.put("context", faqContentResource);
-
-        Prompt prompt = promptTemplate.create(promptMap);
+        Prompt prompt = new Prompt(List.of(systemMessage, assistantMessage, promptMessage));
 
         ChatResponse chatResponse = chatClient
                 .prompt(prompt)
                 .call()
                 .chatResponse();
 
-        System.out.println(chatResponse.getMetadata().getPromptMetadata());
+        System.out.println(chatResponse.getMetadata().getUsage().getPromptTokens());
 
         String response = chatResponse.getResult().getOutput().getText();
 

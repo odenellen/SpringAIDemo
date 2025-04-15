ccc0788b983d1a6a9feb4d2b2231060f4a270c01
amitbattu <amit.battu@vumc.org>
2025-04-15 10:27:38 -0500

    included UIDemo Project

--- src/main/java/com/vumc/SpringAIDemo/OllamaController.java
+++ src/main/java/com/vumc/SpringAIDemo/OllamaController.java
@@ -1,28 +1,32 @@
 package com.vumc.SpringAIDemo;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.AssistantPromptTemplate;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
-import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
-
 @RestController
-@RequestMapping("/api/ollama")
+@RequestMapping("/ollama")
+@CrossOrigin("*")
 public class OllamaController {
+
     private final ChatClient chatClient;
+
+    ChatOptions options;
 
     @Value("classpath:/prompts/prompt.st")
     private Resource promptTemplate;
@@ -36,23 +40,31 @@ public class OllamaController {
     @Value ("classpath:/docs/assistant.txt")
     private Resource assistantText;
 
-    public OllamaController(OllamaChatModel chatModel) {
-        this.chatClient = ChatClient.create(chatModel);
+    public OllamaController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+        options = ChatOptions.builder()
+              .temperature(1.0)
+              .topP(0.9)
+              .frequencyPenalty(0.5)
+              .presencePenalty(0.5)
+              .build();
+
     }
 
-    @GetMapping("/{question}")
-    public ResponseEntity<String> getAnswer(@PathVariable String question) {
+    @GetMapping("/text/{text}")
+    public ResponseEntity<String> getAnswer(@PathVariable(value = "text") String text) {
 
         Message systemMessage = new SystemPromptTemplate(systemText).createMessage();
         Message assistantMessage = new AssistantPromptTemplate(assistantText).createMessage();
 
         PromptTemplate promptTemplate = new PromptTemplate(this.promptTemplate);
-        Message promptMessage = promptTemplate.createMessage(Map.of("question", question, "context", faqContentResource));
+        Message promptMessage = promptTemplate.createMessage(Map.of("question", text, "context", faqContentResource));
 
         Prompt prompt = new Prompt(List.of(systemMessage, assistantMessage, promptMessage));
 
         ChatResponse chatResponse = chatClient
                 .prompt(prompt)
+                .options(options)
                 .call()
                 .chatResponse();
 
@@ -61,5 +73,10 @@ public class OllamaController {
         String response = chatResponse.getResult().getOutput().getText();
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<String> getStatus() {
+        return ResponseEntity.ok("Ok");
     }
 }
